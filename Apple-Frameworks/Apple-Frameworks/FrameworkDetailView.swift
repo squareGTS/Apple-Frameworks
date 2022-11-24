f73e90fade507d348bd6d78a849539d99bab920a
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-11-24 21:47:35 +0300

    add detail view

---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkDetailView.swift
@@ -0,0 +1,53 @@
+//
+//  FrameworkDetailView.swift
+//  Apple-Frameworks
+//
+//  Created by Maxim Bekmetov on 24.11.2022.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var framework: Framework
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .padding()
+            
+            Spacer()
+            
+            FrameworkTitleView(framework: framework)
+            
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+            AFButton(title: "Learn More")
+            }
+        }
+    }
+}
+
+struct FrameworkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkDetailView(framework: MockData.sampleFramework)
+            .preferredColorScheme(.dark)
+    }
+}
