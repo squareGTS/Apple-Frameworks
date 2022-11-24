9874371671ac72eb89c1297e8d9b16afc8b8d32e
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-11-24 18:34:48 +0300

    Add files via upload

---
+++ Apple-Frameworks/Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Maxim Bekmetov on 24.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
